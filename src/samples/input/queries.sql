e0a7e3832f72e93d8f1f567da7de96d842ed2067
Yixuan Jiang <yixuanjiang@dhcp-rhodes-2204.eduroam.cornell.edu>
2018-11-29 23:02:14 -0500

    delete samples

--- src/samples/input/queries.sql
+++
@@ -1,6 +0,0 @@
-SELECT DISTINCT * FROM Sailors;
-SELECT * FROM Sailors, Reserves, Boats WHERE Sailors.A = Reserves.G AND Reserves.H = Boats.D AND Sailors.B < 150;
-SELECT * FROM Sailors S1, Sailors S2 WHERE S1.A < S2.A;
-SELECT B.F, B.D FROM Boats B ORDER BY B.D;
-SELECT * FROM Sailors S, Reserves R, Boats B WHERE S.A = R.G AND R.H = B.D ORDER BY S.C;
-SELECT DISTINCT * FROM Sailors S, Reserves R, Boats B WHERE S.A = R.G AND R.H = B.D ORDER BY S.C;
